4f539fce8f439dfce860063562af6962ba6944ab
Caroline <117231122+carolinefonseca0001@users.noreply.github.com>
2023-06-22 18:33:11 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,40 +2,39 @@
 "use strict";
 
 //Exercise 2
-function parseToJSON(data) {
-  // TODO: Add a try/catch block to
-  // attempt to convert 'data' to JSON
-  return JSON.parse(data);
+// function parseToJSON(data) {
+//   // TODO: Add a try/catch block to
+//   // attempt to convert 'data' to JSON
+//   return JSON.parse(data);
 
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
-}
+//   // TODO: if an exception is raised
+//   // print the error to the console
+//   // and return null
+// }
 
-//Exercise 3
-// TODO: Fix the following parameter list
-function parseToJSON(data) {
+// //Exercise 3
+// // TODO: Fix the following parameter list
+// function parseToJSON(data) {
+//   try {
+//     return JSON.parse(data);
+//   } catch (error) {
+//     console.error;
+//   }
+
+function parseToJSON(value) {
   try {
-    return JSON.parse(data);
+    // let Value = "data";
+    return JSON.parse(value);
   } catch (error) {
-    console.error;
+    console.log(error);
+    // console.log(failData);
+    return null;
   }
-
-  function parseToJSON(value) {
-    try {
-      let Value = "data";
-      return JSON.parse(Value);
-    } catch (error) {
-      console.error;
-      console.log(failData);
-      return null;
-    }
-  }
-  //Exercise 4
-  // TODO: if an exception is raised
-  // print the error to the console
-  // and return null
 }
+//Exercise 4
+// TODO: if an exception is raised
+// print the error to the console
+// and return null
 
 let failData = "<h1>Hello World!</h1>";
 
